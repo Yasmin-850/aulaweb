6a0390a0626f0b4c9a57f88028d371d3414237ca
Yasmin <yasmin.aparecida.estacio@gmail.com>
2025-10-31 21:39:41 -0300

    aula

---
+++ trocaimg.js
@@ -0,0 +1,7 @@
+function mostraCheio(imagem){
+    imagem.src="carrinho-de-compras-removebg-preview.png";
+}
+
+function mostraVazio(imagem){
+    imagem.src="carrinho-de-compras-de-design-xadrez-removebg-preview.png";
+}
